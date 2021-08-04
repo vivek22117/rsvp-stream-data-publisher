fa1b7338f430f13d7080ca1faaefad126ff03c0a
vivek22117 <vivekmishra22117@gmail.com>
2021-08-04 11:59:42 +0530

    updating TF provider

--- aws-infra/ec2-infrastructure/provider.tf
+++ aws-infra/ec2-infrastructure/provider.tf
@@ -1,36 +1,48 @@
+####################################################
+# AWS provider configuration                       #
+####################################################
 provider "aws" {
-  region  = var.default_region // Interpolation Syntax
-  profile = var.profile
+  region = var.default_region
 
-  version = ">=2.22" // AWS plugin version
+  version = ">=2.28.0"
 }
 
-provider "template" {
-  version = "2.1.2"
-}
 
-provider "null" {
-  version = "2.1.2"
-}
-
-provider "random" {
-  version = "2.1.2"
-}
-
-############################################################
-# Terraform configuration block is used to define backend  #
-# Interpolation sytanx is not allowed in Backend           #
-############################################################
+###########################################################
+# Terraform configuration block is used to define backend #
+# Interpolation syntax is not allowed in Backend          #
+###########################################################
 terraform {
-  required_version = ">= 0.12" // Terraform version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.3"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 0.13"
+
 
   backend "s3" {
-    profile        = "admin"
-    bucket         = "doubledigit-tfstate-qa-us-east-1"
-    dynamodb_table = "doubledigit-tfstate-qa-us-east-1"
-    key            = "state/qa/rsvp-collection-tier/ec2/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = "true"
+    region  = "us-east-1"
+    encrypt = "true"
   }
+
 }
 
+data "aws_caller_identity" "current" {} # used for accessing Account ID and ARN
