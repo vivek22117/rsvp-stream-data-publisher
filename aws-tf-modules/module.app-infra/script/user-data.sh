907a2bc67e0b9a6623d5aea3c14a7784bc759591
vivek22117 <vivekmishra22117@gmail.com>
2022-01-09 20:36:13 +0530

    updating user data

--- aws-tf-modules/module.app-infra/script/user-data.sh
+++ aws-tf-modules/module.app-infra/script/user-data.sh
@@ -11,11 +11,6 @@ CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
 $CODEDEPLOY_BIN stop
 yum erase codedeploy-agent -y
 
-echo "Install AWS Cli"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo  ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
-
 cd /home/ec2-user
 
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
