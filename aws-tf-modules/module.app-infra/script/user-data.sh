e5dbdb3912dbe70ba691e8793dc2ab10bb141c53
vivek22117 <vivekmishra22117@gmail.com>
2022-01-09 21:06:41 +0530

    updating user data

--- aws-tf-modules/module.app-infra/script/user-data.sh
+++ aws-tf-modules/module.app-infra/script/user-data.sh
@@ -6,11 +6,6 @@ yum update -y
 sudo yum -y install ruby
 sudo yum install wget unzip curl git -y
 
-CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
-$CODEDEPLOY_BIN stop
-yum erase codedeploy-agent -y
-
-cd /home/ec2-user
 
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
