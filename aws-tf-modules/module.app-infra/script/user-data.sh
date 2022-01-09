aacc5dcb3f7854a2998b7114c97ad62f16880a73
vivek22117 <vivekmishra22117@gmail.com>
2022-01-09 20:27:03 +0530

    updating user data

--- aws-tf-modules/module.app-infra/script/user-data.sh
+++ aws-tf-modules/module.app-infra/script/user-data.sh
@@ -6,17 +6,25 @@ yum update -y
 sudo yum -y install ruby
 sudo yum install wget unzip curl git -y
 
+#!/bin/bash
+CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
+$CODEDEPLOY_BIN stop
+yum erase codedeploy-agent -y
+
 echo "Install AWS Cli"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo  ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
 
-wget -O /tmp/install-codedeploy-agent https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-chmod +x /tmp/install-codedeploy-agent
-sudo /tmp/install-codedeploy-agent auto
-rm /tmp/install-codedeploy-agent
+cd /home/ec2-user
+
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
 
 sudo service codedeploy-agent start
+sudo service codedeploy-agent status
+
 
 echo "export Environment=${environment}" >> /etc/environment
 echo "export LOG_DIR=/opt/rsvp/logs/" >> /etc/environment
