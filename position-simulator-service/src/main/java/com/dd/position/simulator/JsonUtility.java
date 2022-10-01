bd6bedbf84fc7556b182864ed780a978346ec5fc
vivek22117 <vivekmishra22117@gmail.com>
2022-10-01 10:24:08 +0530

    adding Json utility class for json mapping

---
+++ position-simulator-service/src/main/java/com/dd/position/simulator/JsonUtility.java
@@ -0,0 +1,54 @@
+package com.dd.position.simulator;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@Slf4j
+public class JsonUtility {
+
+    private final ObjectMapper objectMapper;
+
+    public JsonUtility() {
+        this(new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+    }
+
+    public JsonUtility(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public <T> String convertToString(T object) throws JsonProcessingException {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException ex) {
+            log.error("Unable to convert into string.." + ex.getMessage());
+        }
+        return null;
+    }
+
+    public <T> T convertFromJson(String json, Class<T> type) throws IOException {
+        try {
+            return objectMapper.readValue(json, type);
+        } catch (IOException ex) {
+            log.error("Unable to covert from json.." + ex.getMessage());
+        }
+        return null;
+    }
+
+    public <T> T convertCollectionFromJson(String json, Class<T> type) throws IOException {
+        TypeFactory typeFactory = objectMapper.getTypeFactory();
+        try {
+            return objectMapper.readValue(json, typeFactory.constructCollectionType(List.class, type));
+        } catch (IOException ex) {
+            log.error("Unable to convert into list..." + ex.getMessage());
+        }
+        return null;
+    }
+}
