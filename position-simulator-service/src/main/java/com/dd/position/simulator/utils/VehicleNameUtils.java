f74cb0a4cac5831e98577856629508f0fb0a5a6d
vivek22117 <vivekmishra22117@gmail.com>
2022-10-02 17:57:12 +0530

    adding vehicle Name util class

---
+++ position-simulator-service/src/main/java/com/dd/position/simulator/utils/VehicleNameUtils.java
@@ -0,0 +1,11 @@
+package com.dd.position.simulator.utils;
+
+import org.apache.commons.text.WordUtils;
+
+public class VehicleNameUtils {
+
+	public static String prettifyName(String originalName) {
+		return WordUtils.capitalizeFully(originalName.replaceAll("_", " "));
+	}
+
+}
