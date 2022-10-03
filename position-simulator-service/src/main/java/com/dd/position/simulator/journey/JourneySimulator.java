83f5389ba4fc90e96f3249a4ac532f8ad16e362f
vivek22117 <vivekmishra22117@gmail.com>
2022-10-03 20:45:12 +0530

    adding api to read data from resources tracks path

--- position-simulator-service/src/main/java/com/dd/position/simulator/journey/JourneySimulator.java
+++ position-simulator-service/src/main/java/com/dd/position/simulator/journey/JourneySimulator.java
@@ -1,11 +1,16 @@
 package com.dd.position.simulator.journey;
 
+import com.dd.position.simulator.utils.PropertyLoaderUtility;
+import com.dd.position.simulator.utils.VehicleNameUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 public class JourneySimulator implements Runnable {
@@ -24,14 +29,41 @@ public class JourneySimulator implements Runnable {
     private void runVehicleSimulation() {
         Map<String, List<String>> reports = new HashMap<>();
         boolean stillRunning = true;
-        while (stillRunning)
-        {
+        while (stillRunning) {
             List<Callable<Object>> calls = new ArrayList<>();
 
-            for (String vehicleName : reports.keySet())
-            {
+            for (String vehicleName : reports.keySet()) {
                 // kick off a message sending thread for this vehicle.
             }
         }
+    }
+
+    /**
+     * Read the data from the resources directory - should work for an executable Jar as
+     * well as through direct execution
+     */
+    private Map<String, List<String>> setUpData() {
+        Map<String, List<String>> reports = new HashMap<>();
+        PathMatchingResourcePatternResolver path = new PathMatchingResourcePatternResolver();
+
+        try {
+            for (Resource nextFile : path.getResources("tracks/*")) {
+                URL resource = nextFile.getURL();
+                File f = new File(resource.getFile());
+                String vehicleName = VehicleNameUtils.prettifyName(f.getName());
+                Optional<List<String>> vehicleReports = PropertyLoaderUtility.getInstance().getVehiclePositions("/tracks/" + f.getName());
+
+                vehicleReports.ifPresent(value -> {
+                    reports.put(vehicleName, value);
+                });
+            }
+            return Collections.unmodifiableMap(reports);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void finish() {
+        threadPool.shutdown();
     }
 }
