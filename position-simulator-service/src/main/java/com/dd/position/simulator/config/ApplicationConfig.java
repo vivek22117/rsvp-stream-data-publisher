5decf0194a4a60cba9c21c44cf94ed482bcfd141
vivek22117 <vivekmishra22117@gmail.com>
2022-10-09 10:10:12 +0530

    adding application properties for local profile and updating thread configuration class

--- position-simulator-service/src/main/java/com/dd/position/simulator/config/ApplicationConfig.java
+++ position-simulator-service/src/main/java/com/dd/position/simulator/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.dd.position.simulator.config;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -15,9 +16,14 @@ import java.util.concurrent.Executors;
 @Slf4j
 public class ApplicationConfig {
 
-    private static final int MAX_POOL_SIZE = 2;
-    private static final int CORE_POOL_SIZE = 2;
-    private static final int QUEUE_CAPACITY = 500;
+    @Value("${threadpool.corepoolsize}")
+    int corePoolSize;
+
+    @Value("${threadpool.maxpoolsize}")
+    int maxPoolSize;
+
+    @Value("${threadpool.queuesize}")
+    int queueSize;
 
 
     @Bean
@@ -28,9 +34,9 @@ public class ApplicationConfig {
     @Bean
     public ThreadPoolTaskExecutor createExecutorService() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(MAX_POOL_SIZE);
-        executor.setMaxPoolSize(CORE_POOL_SIZE);
-        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueSize);
         executor.setThreadNamePrefix("JDAsync-");
         executor.initialize();
         return executor;
