79802e8330c14eb40f732fd42ab901958e275883
vivek22117 <vivekmishra22117@gmail.com>
2022-10-09 09:59:33 +0530

    adding constant for Thread pool executor configurations

--- position-simulator-service/src/main/java/com/dd/position/simulator/config/ApplicationConfig.java
+++ position-simulator-service/src/main/java/com/dd/position/simulator/config/ApplicationConfig.java
@@ -1,9 +1,8 @@
 package com.dd.position.simulator.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.slf4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -12,13 +11,14 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executors;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
 @Configuration
+@Slf4j
 public class ApplicationConfig {
 
-    private static final Logger logger = getLogger(ApplicationConfig.class);
-    private static AWSCredentialsProvider awsCredentialsProvider;
+    private static final int MAX_POOL_SIZE = 2;
+    private static final int CORE_POOL_SIZE = 2;
+    private static final int QUEUE_CAPACITY = 500;
+
 
     @Bean
     public Gson createGson() {
@@ -28,16 +28,16 @@ public class ApplicationConfig {
     @Bean
     public ThreadPoolTaskExecutor createExecutorService() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(500);
+        executor.setCorePoolSize(MAX_POOL_SIZE);
+        executor.setMaxPoolSize(CORE_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
         executor.setThreadNamePrefix("JDAsync-");
         executor.initialize();
         return executor;
     }
 
     @Bean(name = "ConcurrentTaskExecutor")
-    public TaskExecutor taskExecutor2 () {
+    public TaskExecutor taskExecutor2() {
         return new ConcurrentTaskExecutor(
                 Executors.newFixedThreadPool(3));
     }
