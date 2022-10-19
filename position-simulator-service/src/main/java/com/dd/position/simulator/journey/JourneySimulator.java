e0b60f97236d580ace93fb3d00afad85c13ce71b
vivek22117 <vivekmishra22117@gmail.com>
2022-10-19 21:23:47 +0530

    runnable class to trigger journey data creation

--- position-simulator-service/src/main/java/com/dd/position/simulator/journey/JourneySimulator.java
+++ position-simulator-service/src/main/java/com/dd/position/simulator/journey/JourneySimulator.java
@@ -1,39 +1,58 @@
 package com.dd.position.simulator.journey;
 
+import com.dd.position.simulator.publisher.PositionPublisher;
+import com.dd.position.simulator.utils.PropertyLoaderUtility;
 import com.dd.position.simulator.utils.VehicleNameUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static com.dd.position.simulator.utils.PropertyLoaderUtility.getInstance;
 
+@Component
+@Slf4j
 public class JourneySimulator implements Runnable {
 
-    private final ThreadPoolTaskExecutor threadPool;
+    private ExecutorService threadPool;
 
-    public JourneySimulator(ThreadPoolTaskExecutor threadPool) {
-        this.threadPool = threadPool;
+    public JourneySimulator() {
     }
+
 
     @Override
     public void run() {
-        this.runVehicleSimulation();
+        try {
+            this.runVehicleSimulation();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
-    private void runVehicleSimulation() {
+    private void runVehicleSimulation() throws InterruptedException {
         Map<String, List<String>> reports = setUpData();
+        threadPool = Executors.newCachedThreadPool();
         boolean stillRunning = true;
+
         while (stillRunning) {
+
             List<Callable<Object>> calls = new ArrayList<>();
 
             for (String vehicleName : reports.keySet()) {
-                // kick off a message sending thread for this vehicle.
+                calls.add(new Journey(new PositionPublisher(), reports.get(vehicleName), vehicleName));
+            }
+            threadPool.invokeAll(calls);
+            if (threadPool.isShutdown()) {
+                stillRunning = false;
             }
         }
     }
@@ -46,12 +65,13 @@ public class JourneySimulator implements Runnable {
         Map<String, List<String>> reports = new HashMap<>();
         PathMatchingResourcePatternResolver path = new PathMatchingResourcePatternResolver();
 
+        PropertyLoaderUtility propertyLoader = getInstance();
         try {
             for (Resource nextFile : path.getResources("tracks/*")) {
                 URL resource = nextFile.getURL();
                 File f = new File(resource.getFile());
                 String vehicleName = VehicleNameUtils.prettifyName(f.getName());
-                Optional<List<String>> vehicleReports = getInstance().getVehiclePositions("/tracks/" + f.getName());
+                Optional<List<String>> vehicleReports = propertyLoader.getVehiclePositions("/tracks/" + f.getName());
 
                 vehicleReports.ifPresent(value -> {
                     reports.put(vehicleName, value);
