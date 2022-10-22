44170d78cc464688084c36d1b12bf70c949f91b8
vivek22117 <vivekmishra22117@gmail.com>
2022-10-22 21:08:30 +0530

    updating main class

--- position-simulator-service/src/main/java/com/dd/position/simulator/PositionSimulatorApp.java
+++ position-simulator-service/src/main/java/com/dd/position/simulator/PositionSimulatorApp.java
@@ -2,8 +2,10 @@ package com.dd.position.simulator;
 
 
 import com.dd.position.simulator.config.SwaggerConfig;
+import com.dd.position.simulator.journey.JourneySimulator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
@@ -11,6 +13,11 @@ import org.springframework.context.annotation.Import;
 public class PositionSimulatorApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(PositionSimulatorApp.class, args);
+        try (ConfigurableApplicationContext ctx = SpringApplication.run(PositionSimulatorApp.class)) {
+            final JourneySimulator simulator = ctx.getBean(JourneySimulator.class);
+
+            Thread mainThread = new Thread(simulator);
+            mainThread.start();
+        }
     }
 }
